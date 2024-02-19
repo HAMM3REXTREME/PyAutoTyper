0da1fdf0de0327122e778e70f4eaeffec8cd8a07
Hemanya Sharma <hemanya.sharma@zoho.com>
2024-02-18 23:07:15 -0500

    Refactor

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import pyautogui
 import time
 
-
 def read_file(filename):
-    f = open(filename, "r")
-    stuff = f.read()
-    f.close()
-    return stuff
-
+    with open(filename, "r") as f:
+        return f.read()
 
 def type_text(start, sentence):
+    words = sentence.split()
     wc = start
     try:
-        for word in sentence.split(" ")[wc:]:
+        for i, word in enumerate(words, start=start):
             pyautogui.write(word, interval=0.10)
-            pyautogui.write(" ")
-            wc += 1
-            print("Typing word #" + str(wc) + " is '" + word + "'")
+            pyautogui.press("space")
+            print(f"Typing word #{i}: {word}")
+            wc = i
             time.sleep(0.2)
         return wc
     except KeyboardInterrupt:
         return -wc
-
 
 def main():
     progress = 0
@@ -29,16 +25,16 @@ def main():
         text = read_file("answer.txt")
     except FileNotFoundError:
         print("The file does not exist. Make one!\n")
-        exit(1)
+        return
+
     print("Will begin typing in 3 seconds...\n")
     time.sleep(3)
-    progress = type_text(progress, text)
 
+    progress = type_text(progress, text)
     if progress > 0:
         print("\nFinished typing all " + str(progress) + " words.\n")
     else:
         print('\nRemember to continue at word #' + str(-progress) + "...\n")
-
 
 if __name__ == '__main__':
     main()
